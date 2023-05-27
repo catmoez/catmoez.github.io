3d49636562192dcd4c416f2b7e30c891ceadf3c8
Catherine Moez <75140908+catmoez@users.noreply.github.com>
2023-05-27 19:32:12 -0400

    add bulldog images

---
+++ functions.js
@@ -0,0 +1,14 @@
+// rotate between images
+
+// print a random image from a list of images
+function printRandomImg() {
+    var bulldog_imgs = [
+        `<img src = "bulldog_images/bulldog1.png" alt = "Winston the bulldog">`,
+        `<img src = "bulldog_images/bulldog2.jpg" alt = "Bulldog in a hat">`,
+        `<img src = "bulldog_images/bulldog3.jpg" alt = "angry chorkie">`,
+        `<img src = "bulldog_images/bulldog4.jpg" alt = "Favourite bulldog">`
+    ];
+    var random_dogimg = bulldog_imgs[Math.floor(Math.random() * bulldog_imgs.length)];
+    // print the random IMAGE
+    document.write(random_dogimg);
+}
