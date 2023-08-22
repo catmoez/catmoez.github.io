f58e57ce21259153753ccffdcaa03cadb1e550c4
Catherine Moez <75140908+catmoez@users.noreply.github.com>
2023-08-22 17:30:50 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -40,7 +40,7 @@ function printRandomPopImg() {
         `<img src = "populist_images/getty images - javier milei 2023.jpg" style="width: 1000px; height: 700px;" alt = "Getty Images 2023 (https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn1.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcQN7X_MCYhWz9IBVljsGI1WGmWWYhZ25_P_roRNqnFVn41vTwbraR295Xnbt2MCdtiKQYhKye99rQ8TH9Q&psig=AOvVaw0QDhyHdaM3Rkq6LpGaL8Qt&ust=1692822260004000&source=images&cd=vfe&ved=0CBEQjhxqFwoTCLiKw8iM8YADFQAAAAAdAAAAABAD): Javier Milei, Argentina">`,
         `<img src = "populist_images/florianbasedmortimer rain epler.jpg" alt = "Twitter 2023 (https://twitter.com/BasedMortimer/status/1632461155988918276): Right-nationalist Rainn Epler, Estonia">`
     ];
-    var nbr = Math.floor(Math.random() * populist_imgs.length)
+    var nbr = Math.floor(Math.random() * populist_imgs.length);
     var random_popimg = populist_imgs[nbr];
     // print the random IMAGE
     document.write(random_popimg);
