9fd71c771ac4b8bf762c99b2a444f7363e64d888
Catherine Moez <75140908+catmoez@users.noreply.github.com>
2023-08-22 17:12:56 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -19,8 +19,8 @@ function printRandomImg() {
 function printRandomPopImg() {
     var populist_imgs = [
         `<img src = "populist_images/ch1 FT 2014 UKIP ad campaign.jpg" alt = "FT 2014 - UKIP European Parliament campaign">`,
-        `<img src = "populist_images/ch1 ukip ad campaign celebrity lifestyle eurocrat campaignlive 2014.jpg" alt = "CampaignLive 2014 - UKIP European Parliament campaign">`,
-        `<img src = "populist_images/ch1 ukip ad campaign who runs the country campaignlive 2014.jpg" alt = "CampaignLive 2014 - UKIP European Parliament campaign">`,
+        `<img src = "populist_images/ch1 ukip ad campaign celebrity lifestyle eurocrat campaignlive 2014.jpeg" alt = "CampaignLive 2014 - UKIP European Parliament campaign">`,
+        `<img src = "populist_images/ch1 ukip ad campaign who runs the country campaignlive 2014.jpeg" alt = "CampaignLive 2014 - UKIP European Parliament campaign">`,
         `<img src = "populist_images/a-young-rt-hon-tony-blair-mp-helps-launch-a-1992-general-election-BJ350W.jpg" alt = "Alamy (https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.alamy.com%2Fstock-photo-a-young-rt-hon-tony-blair-mp-helps-launch-a-1992-general-election-28343977.html&psig=AOvVaw1uSJ7SKKkSBmOE3MypkFYD&ust=1692820138463000&source=images&cd=vfe&opi=89978449&ved=0CBIQjhxqFwoTCOCjl8qE8YADFQAAAAAdAAAAABAE) - Tony Blair 1992: Majorism isn't working">`,
         `<img src = "populist_images/new labour1.jpg" alt = "Yahoo Finance (https://www.google.com/url?sa=i&url=https%3A%2F%2Fca.finance.yahoo.com%2Fnews%2Fnothing-really-working-anymore-britain-094121292.html&psig=AOvVaw1uSJ7SKKkSBmOE3MypkFYD&ust=1692820138463000&source=images&cd=vfe&opi=89978449&ved=0CBIQjhxqFwoTCOCjl8qE8YADFQAAAAAdAAAAABAJ) - Tony Blair 1992: Majorism isn't working">`,
         `<img src = "populist_images/new labour mug 1 buzzfeed.png" alt = "Buzzfeed: New Labour campaign mugs, 1997">`,
@@ -38,7 +38,7 @@ function printRandomPopImg() {
         `<img src = "populist_images/rhodes purdy et al 2023 p11 spread and intensification of discontentment parties.png" alt = "Rhodes-Purdy et al. 2023: Rise of discontentment parties">`,
         `<img src = "populist_images/verne el pais - pamela jilas.jpg" alt = "Verne el Pais 2020 (https://www.google.com/url?sa=i&url=https%3A%2F%2Fverne.elpais.com%2Fverne%2F2020%2F07%2F19%2Fmexico%2F1595114669_072603.html&psig=AOvVaw3MNoOcm2q8AS3sCZri1WmL&ust=1692822230441000&source=images&cd=vfe&opi=89978449&ved=0CBIQjhxqFwoTCNiw4q-M8YADFQAAAAAdAAAAABAE): Pamela Jilas, Chile">`,
         `<img src = "populist_images/getty images - javier milei 2023.jpg" alt = "Getty Images 2023 (https://www.google.com/url?sa=i&url=https%3A%2F%2Fencrypted-tbn1.gstatic.com%2Flicensed-image%3Fq%3Dtbn%3AANd9GcQN7X_MCYhWz9IBVljsGI1WGmWWYhZ25_P_roRNqnFVn41vTwbraR295Xnbt2MCdtiKQYhKye99rQ8TH9Q&psig=AOvVaw0QDhyHdaM3Rkq6LpGaL8Qt&ust=1692822260004000&source=images&cd=vfe&ved=0CBEQjhxqFwoTCLiKw8iM8YADFQAAAAAdAAAAABAD): Javier Milei, Argentina">`,
-        `<img src = "populist_images/florianbasedmortimer rain epler.jpg" alt = "Twitter (@BasedMortimer) 2023 (https://twitter.com/BasedMortimer/status/1632461155988918276): Right-nationalist Rainn Epler, Estonia">`
+        `<img src = "populist_images/florianbasedmortimer rain epler.jpg" alt = "Twitter 2023 (https://twitter.com/BasedMortimer/status/1632461155988918276): Right-nationalist Rainn Epler, Estonia">`
     ];
     var random_popimg = populist_imgs[Math.floor(Math.random() * populist_imgs.length)];
     // print the random IMAGE
