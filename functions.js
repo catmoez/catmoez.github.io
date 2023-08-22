e2febf29cedee074ae1a98dd254af16096971d5c
Catherine Moez <75140908+catmoez@users.noreply.github.com>
2023-08-22 15:32:47 -0400

    Update functions.js

--- functions.js
+++ functions.js
@@ -12,3 +12,18 @@ function printRandomImg() {
     // print the random IMAGE
     document.write(random_dogimg);
 }
+
+
+
+// Aug. 22, 2023: Print political images
+function printRandomPopImg() {
+    var populist_imgs = [
+        `<img src = "bulldog_images/bulldog1.png" alt = "Winston the bulldog">`,
+        `<img src = "bulldog_images/bulldog2.jpg" alt = "Bulldog in a hat">`,
+        `<img src = "bulldog_images/bulldog3.jpg" alt = "angry chorkie">`,
+        `<img src = "bulldog_images/bulldog4.jpg" alt = "Favourite bulldog">`
+    ];
+    var random_dogimg = bulldog_imgs[Math.floor(Math.random() * bulldog_imgs.length)];
+    // print the random IMAGE
+    document.write(random_dogimg);
+}
